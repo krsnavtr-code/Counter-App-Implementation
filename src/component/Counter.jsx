662d5dae248cf207beabd9a5a8121262972d8b93
Krishna Avtar <113005173+krsnavtr-code@users.noreply.github.com>
2024-02-29 08:55:09 +0530

    initial commit

---
+++ src/component/Counter.jsx
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+import styles from "./Counter.module.css";
+
+const Counter = () => {
+  const [count, setCount] = useState(0);
+
+  const handleIncrement = () => {
+    setCount((prevCount) => prevCount + 1);
+  };
+
+  const handleDecrement = () => {
+    setCount((prevCount) => prevCount - 1);
+  };
+
+  return (
+    <div className={styles.container}>
+      <h1>Counter App</h1>
+      <p>Count: {count}</p>
+      <div>
+        <button onClick={handleIncrement}>Increment</button>
+        <button onClick={handleDecrement}>Decrement</button>
+      </div>
+    </div>
+  );
+};
+
+export default Counter;
